9e67c9d5ea3d52897bd163c3f38d8aa0781265b2
chris wayne <yvesngah95@gmail.com>
2020-04-15 14:44:27 -0930

    adding ssl to database_url

--- patryarch/settings.py
+++ patryarch/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'patryarch.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES={
-     'default' : dj_database_url.config(conn_max_age=600)
+     'default' : dj_database_url.config(conn_max_age=600, ssl_require=True)
      }
 
 # Password validation
