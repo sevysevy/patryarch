00de890654d35b89810e1f92f2649803f941da3f
chris wayne <yvesngah95@gmail.com>
2020-04-20 05:19:49 -0930

    20/04/2020 3

--- patryarch/settings.py
+++ patryarch/settings.py
@@ -118,10 +118,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+class DjsManifestStaticFilesStorage(ManifestStaticFilesStorage):
+    manifest_strict = False
+
+    
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedDjsManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
