29c36fe620b7bd2f1902afdb26032b3744aba5c1
chris wayne <yvesngah95@gmail.com>
2020-04-21 13:17:12 -0930

    fix static

--- patryarch/storage.py
+++ patryarch/storage.py
@@ -3,3 +3,11 @@ from whitenoise.storage import CompressedManifestStaticFilesStorage
 
 class WhiteNoiseStaticFilesStorage(CompressedManifestStaticFilesStorage):
     manifest_strict = False
+    
+    def hashed_name(self, name, content=None, filename=None):
+        try:
+            result = super().hashed_name(name, content, filename)
+        except ValueError:
+            # When the fille is missing, let's forgive and ignore that.
+            result = name
+        return result
