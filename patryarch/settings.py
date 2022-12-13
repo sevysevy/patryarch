984b2cf2ec25c1abe3587c0c414a70083f3fed10
sevy <ngahsevy@gmail.com>
2022-12-13 03:35:41 +0100

    sqlite

--- patryarch/settings.py
+++ patryarch/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-7x2nzrajo6c7w^$v5mhl^)g5u+#18k8y+@fw7s3i_oz5)7b7#g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -89,9 +89,12 @@ WSGI_APPLICATION = 'patryarch.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES={
-     'default' : dj_database_url.config(conn_max_age=600, ssl_require=True)
-     }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
